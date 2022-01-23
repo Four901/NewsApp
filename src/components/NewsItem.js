693aa337f562d2a5b13611c932fa7f691f69e00e
Ashish Meena <akm411201@gmail.com>
2022-01-23 15:35:40 +0530

    author bug

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -11,7 +11,7 @@ const NewsItem =(props)=> {
         <div className="card-body">
       <h5 className="card-title">{title}...</h5>
      <p className="card-text">{description}</p>
-     <p className="card-text"><small className="text-muted">By {author?"Unknown":author} at {datetime}</small></p>
+     <p className="card-text"><small className="text-muted">By {author==null?"Unknown":author} at {datetime}</small></p>
      <a href={ newsUrl/*`/newsDetails/${newsUrl}`*/} className="btn btn-dark">Read More</a>
       </div>
 </div>
